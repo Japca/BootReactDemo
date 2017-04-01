9005a11c4a91148cfad1d69f58b6895a3bd39ff3
Japca <he4loGIT_1>
2017-04-01 15:46:31 +0200

    added webpack + react

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Layout from "./component/Layout";
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
