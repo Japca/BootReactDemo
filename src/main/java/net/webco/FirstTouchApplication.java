b165ee47766acafa60e288653adbdc7c2a1e239b
Japca <he4loGIT_1>
2017-04-01 09:52:23 +0200

    init

---
+++ src/main/java/net/webco/FirstTouchApplication.java
@@ -0,0 +1,12 @@
+package net.webco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTouchApplication.class, args);
+	}
+}
