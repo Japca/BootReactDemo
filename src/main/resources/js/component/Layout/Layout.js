eb9282c5ad1383c2e86815d885f2b7a794c9140a
Japca <he4loGIT_1>
2017-07-21 21:15:04 +0200

    Moved store

--- src/main/resources/js/component/Layout/Layout.js
+++ src/main/resources/js/component/Layout/Layout.js
@@ -3,12 +3,6 @@ import Navigator from '../Navigator/Navigator';
 import Content from '../Content/Content';
 import { BrowserRouter as Router } from 'react-router-dom';
 import styles from './layout.css';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { reducers } from '../../Reducer/index.js';
-import ReduxPromise from 'redux-promise';
-
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 export const PAGE_LIST = '/list';
 export const PAGE_TABLE = '/table';
@@ -19,14 +13,13 @@ export default class Layout extends Component {
     render() {
         return (
             <div className={styles.mainLayout}>
-                <Provider store={createStoreWithMiddleware(reducers)}>
+
                     <Router>
                         <div>
                             <Navigator/>
                             <Content />
                         </div>
                     </Router>
-                </Provider>
             </div>
         );
     }
--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -2,8 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./component/Layout/Layout";
 // import "react-rpm";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { reducers } from './Reducer/index.js';
+import ReduxPromise from 'redux-promise';
+
+
+
+const createStoreWithMiddleware = compose(
+    applyMiddleware(ReduxPromise),
+    window.devToolsExtension ? window.devToolsExtension() : f => f)
+(createStore);
+
+const store = createStoreWithMiddleware(reducers);
+export default store;
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout />,   app);
+ReactDOM.render(
+    <Provider store={store}>
+       <Layout />
+    </Provider>,
+    app);
+
+
 
 
