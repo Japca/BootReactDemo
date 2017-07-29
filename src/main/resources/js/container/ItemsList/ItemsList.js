bb982d05c35698090b0574e4ded6a549b8902fd0
Japca <he4loGIT_1>
2017-07-29 18:02:08 +0200

    Added tooltips and animations2

--- src/main/resources/js/container/ItemsList/ItemsList.js
+++ src/main/resources/js/container/ItemsList/ItemsList.js
@@ -30,21 +30,24 @@ class ItemsList extends Component {
                     <img width={128} height={128} src={character.image}/>
                 </div>
 
-                <div className={styles.buttonLayout}>
-                    <a className={[styles.itemButton, "showTooltip"].join(' ')}  title="Update Character" onClick={() => this.props.initForm(character)}>
-                       <i className='fa fa-pencil fa-lg'/>
-                    </a>
-                       <a className={[styles.itemButton, "showTooltip"].join(' ')} title="Delete Character"onClick={() => this.props.deleteCharacter(character)}>
-                       <i className='fa fa-times fa-lg'/>
-                    </a>
-                </div>
-
-                <div className={styles.content}>
+                 <div className={styles.content}>
                     <h3> {character.name}</h3>
                     <h4>{character.profession}</h4>
                     <p>{character.description}</p>
-                    <p>{character.email}</p>
+                    <div className={styles.email}>  <p>{character.email}</p> </div>
                 </div>
+
+                <div className={styles.buttonLayout}>
+                    <a className={[styles.itemButton, "showTooltip"].join(' ')} title="Update Character"
+                       onClick={() => this.props.initForm(character)}>
+                        <i className='fa fa-pencil fa-lg'/>
+                    </a>
+                    <a className={[styles.itemButton, "showTooltip"].join(' ')} title="Delete Character"
+                       onClick={() => this.props.deleteCharacter(character)}>
+                        <i className='fa fa-times fa-lg'/>
+                    </a>
+                </div>
+
             </div>;
         });
     };
